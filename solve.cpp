8a50f2dc96853812ef058d9d66ea473ae36f0c5d
Parag Karanjekar <98255049+paragkaranjekar@users.noreply.github.com>
2023-07-07 23:51:52 +0530

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -1,95 +1,89 @@
-#include <iostream>
-#include <vector>
 #include <queue>
-#include <climits>
 
-using namespace std;
+const int INF = 1000000;
+const int MAX_ROWS = 10;
+const int MAX_COLS = 10;
 
-// Structure to represent a cell in the maze
 struct Cell {
-    int row;
-    int col;
-    int distance;
+  int row;
+  int col;
+  int distance;
 
-    Cell(int r, int c, int d) : row(r), col(c), distance(d) {}
+  Cell(int r, int c, int d) : row(r), col(c), distance(d) {}
 };
 
-// Helper function to calculate the Manhattan distance between two cells
-int manhattanDistance(const Cell& cell1, const Cell& cell2) {
-    return abs(cell1.row - cell2.row) + abs(cell1.col - cell2.col);
-}
+int dijkstra(char maze[MAX_ROWS][MAX_COLS], Cell start, Cell end) {
+  int rows = MAX_ROWS;
+  int cols = MAX_COLS;
 
-// Function to find the shortest path using Dijkstra's algorithm
-int dijkstra(vector<vector<char>>& maze, const Cell& start, const Cell& end) {
-    int rows = maze.size();
-    int cols = maze[0].size();
+  int distances[MAX_ROWS][MAX_COLS];
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      distances[i][j] = INF;
+    }
+  }
+  distances[start.row][start.col] = 0;
 
-    // Initialize distances with infinity
-    vector<vector<int>> distances(rows, vector<int>(cols, INT_MAX));
-    distances[start.row][start.col] = 0;
+  std::priority_queue<Cell, std::vector<Cell>, std::greater<Cell>> pq;
+  pq.push(start);
 
-    // Priority queue for Dijkstra's algorithm
-    priority_queue<Cell, vector<Cell>, function<bool(const Cell&, const Cell&)>> pq([](const Cell& c1, const Cell& c2) {
-        return c1.distance > c2.distance;
-    });
+  while (!pq.empty()) {
+    Cell currentCell = pq.top();
+    pq.pop();
 
-    // Add the start cell to the priority queue
-    pq.push(start);
-
-    while (!pq.empty()) {
-        Cell currentCell = pq.top();
-        pq.pop();
-
-        // Check if we reached the end point
-        if (currentCell.row == end.row && currentCell.col == end.col) {
-            return distances[end.row][end.col];
-        }
-
-        // Explore neighbors
-        vector<Cell> neighbors = {
-            Cell(currentCell.row - 1, currentCell.col, currentCell.distance + 1),
-            Cell(currentCell.row + 1, currentCell.col, currentCell.distance + 1),
-            Cell(currentCell.row, currentCell.col - 1, currentCell.distance + 1),
-            Cell(currentCell.row, currentCell.col + 1, currentCell.distance + 1)
-        };
-
-        for (const Cell& neighbor : neighbors) {
-            int row = neighbor.row;
-            int col = neighbor.col;
-
-            // Check if the neighbor is within the maze bounds
-            if (row >= 0 && row < rows && col >= 0 && col < cols) {
-
-                // Calculate the new distance to the neighbor
-                int newDistance = currentCell.distance + manhattanDistance(currentCell, neighbor);
-
-                // Update the distance if it's shorter
-                if (newDistance < distances[row][col] && maze[row][col] != '#') {
-                    distances[row][col] = newDistance;
-                    pq.push(Cell(row, col, newDistance));
-                }
-            }
-        }
+    if (currentCell.row == end.row && currentCell.col == end.col) {
+      return distances[end.row][end.col];
     }
 
-    // No path found
-    return -1;
-}
-
-int main() {
-    vector<vector<char>> maze = {
-        {'S', '.', '.', '#', '.', '.', '.'},
-        {'.', '#', '.', '#', '.', '#', '.'},
-        {'.', '#', '.', '.', '.', '.', '.'},
-        {'.', '.', '#', '#', '#', '.', '.'},
-        {'.', '#', '#', 'E', '#', '#', '.'},
+    Cell neighbors[4] = {
+      Cell(currentCell.row - 1, currentCell.col, currentCell.distance + 1),
+      Cell(currentCell.row + 1, currentCell.col, currentCell.distance + 1),
+      Cell(currentCell.row, currentCell.col - 1, currentCell.distance + 1),
+      Cell(currentCell.row, currentCell.col + 1, currentCell.distance + 1)
     };
 
-    Cell start(0, 0, 0);  // Start position (row, column, distance)
-    Cell end(4, 3, 0);    // End position (row, column, distance)
+    for (int i = 0; i < 4; i++) {
+      int row = neighbors[i].row;
+      int col = neighbors[i].col;
 
-    int shortestDistance = dijkstra(maze, start, end);
-    cout << "Shortest distance: " << shortestDistance << endl;
+      if (row >= 0 && row < rows && col >= 0 && col < cols) {
+        int newDistance = currentCell.distance + 1;
 
-    return 0;
+        if (newDistance < distances[row][col] && maze[row][col] != '#') {
+          distances[row][col] = newDistance;
+          pq.push(Cell(row, col, newDistance));
+        }
+      }
+    }
+  }
+
+  return -1;
+}
+
+void setup() {
+  // Initialize maze
+  char maze[MAX_ROWS][MAX_COLS] = {
+    {'S', '.', '.', '#', '.', '.', '.', '.', '.', '.'},
+    {'.', '#', '.', '#', '.', '#', '.', '#', '#', '.'},
+    {'.', '#', '.', '.', '.', '.', '.', '#', '.', '.'},
+    {'.', '#', '#', '#', '#', '#', '.', '#', '.', '.'},
+    {'.', '.', '.', '.', '.', '.', '.', '#', '.', '#'},
+    {'.', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+    {'.', '#', '.', '.', '.', '#', '.', '.', '.', '#'},
+    {'.', '#', '#', '#', '.', '#', '#', '#', '#', '#'},
+    {'.', '.', '.', '#', '.', '#', '.', '.', '.', '.'},
+    {'.', '#', '#', 'E', '.', '#', '#', '#', '#', '.'}
+  };
+
+  Cell start(0, 0, 0);  // Start position (row, column, distance)
+  Cell end(9, 3, 0);    // End position (row, column, distance)
+
+  int shortestDistance = dijkstra(maze, start, end);
+  Serial.begin(9600);
+  Serial.print("Shortest distance: ");
+  Serial.println(shortestDistance);
+}
+
+void loop() {
+  // Your code here
 }
